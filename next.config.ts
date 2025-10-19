becda286440975da3aeb12e63fb5ff7e0659d251
heytrackid <contact@heytrack.id>
2025-10-19 14:52:35 +0700

    deep clean: completely remove Sentry from project
    - Removed all instrumentation files (instrumentation.ts, instrumentation-client.ts, instrumentation-server.ts)
    - Removed Sentry configuration from next.config.ts (withSentryConfig wrapper)
    - Removed @sentry/nextjs from package.json dependencies
    - Removed .env.sentry-build-plugin file
    - Cleaned up all Sentry-related configurations and imports
    - Project now runs without any Sentry dependencies or configurations

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-import {withSentryConfig} from '@sentry/nextjs';
 import type { NextConfig } from "next";
 import BundleAnalyzer from '@next/bundle-analyzer';
 
@@ -190,35 +189,4 @@ const nextConfig: NextConfig = {
 // Enable bundle analyzer when ANALYZE=true
 const configWithAnalyzer = withBundleAnalyzer(nextConfig);
 
-export default withSentryConfig(configWithAnalyzer, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
-  org: "heytrack",
-
-  project: "javascript-nextjs",
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: "/error-monitoring",
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
-});
+export default configWithAnalyzer;
